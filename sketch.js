2a03cfa7f7d3a102c5e2f21d8ebc5bd31e6c5893
Sagnik <74058957+ElonMusk-AI@users.noreply.github.com>
2020-12-29 19:18:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,29 +2,70 @@ var hr,mi,se;
 var hrAngle,miAngle,seAngle;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(600,800);
+}
+
+function draw() {
+  background(0);  
   hr = hour();
   mi = minute();
   se = second();
 
   angleMode(DEGREES);
   
-  hrAngle = map(hr,0,24,0,360)
+  hrAngle = map(hr%12,0,12,0,360)
   miAngle = map(mi,0,60,0,360)
   seAngle = map(se,0,60,0,360)
 
+  stroke("red")
+  strokeWeight(10);
+  noFill();
+  arc(300, 400, 440, 440, -90,  seAngle);
+
+  stroke(color(0,255,0))
+  strokeWeight(10);
+  noFill();
+  arc(300, 400, 400, 400, -90,  miAngle);
+
+  stroke("blue")
+  strokeWeight(10);
+  noFill();
+  arc(300, 400, 360, 360, -90,  hrAngle);
+
+  hourHand();
+  minuteHand();
+  secondHand();
+
+  
+  drawSprites();
 }
 
-function draw() {
-  background(0);  
-  //hour hands
+
+function hourHand(){
   push();
-  translate()
+  translate(300,400)
   rotate(hrAngle);
   stroke("blue");
-  strokeWeight(5);
+  strokeWeight(10);
   line(0,0,100,0);
   pop();
-
-  drawSprites();
+}
+function minuteHand(){
+  push();
+  translate(300,400)
+  rotate(miAngle);
+  stroke(0,255,0);
+  strokeWeight(10);
+  line(0,0,125,0);
+  pop();
+}
+function secondHand(){
+  push();
+  translate(300,400)
+  rotate(seAngle);
+  stroke("red");
+  strokeWeight(10);
+  line(0,0,150,0);
+  
+  pop();
 }
